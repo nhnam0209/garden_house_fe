cf93ea79f292caeb07e7dec639d97a2bb2c72edb
nhnam3004 <nhnam3004@gmail.com>
2023-06-27 17:27:31 +0700

    Add Nav desktop

---
+++ components/organisms/NavigationBarDesktop.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Navigation bar Desktop</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot name="header">
+      <navigation-bar-desktop />
+    </slot>
+    <slot name="content">
+      <Nuxt />
+    </slot>
+    <slot name="footer">
+      <Footer />
+    </slot>
+  </div>
+</template>
+<script lang="ts"></script>
